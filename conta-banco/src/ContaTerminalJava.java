a9451d741cfff01f40623e3334aeea7de7e96fb2
Maycon <mayconleite06@yahoo.com.br>
2024-10-07 23:51:36 -0300

    Execusão da mensagem - Finalizado

--- conta-banco/src/ContaTerminalJava.java
+++ conta-banco/src/ContaTerminalJava.java
@@ -1,24 +1,27 @@
 
 import java.util.Locale;
 import java.util.Scanner;
+
 public class ContaTerminalJava {
     public static void main(String[] args) {
+        // Dado armazenado da conta
         Double saldo = 0.02;
 
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite seu nome");
-        String nome = scanner.next();
+            System.out.println("Digite seu nome");
+            String nome = scanner.next();
 
-        System.out.println("Digite sua Agencia");
-        String agencia = scanner.next();
+            System.out.println("Digite sua Agencia");
+            String agencia = scanner.next();
     
         
-        System.out.println("Digite sua conta");
-        String conta = scanner.next();
+            System.out.println("Digite sua conta");
+            String contaBancaria = scanner.next();
 
-        
-
+        System.out.println("Olá " + nome +", obrigado por criar uma conta em nosso banco.");
+        System.out.println("sua agência é " + agencia+", conta " + contaBancaria +
+        " e seu saldo "+ saldo +" já está disponível para saque.");
 
     }
 }
