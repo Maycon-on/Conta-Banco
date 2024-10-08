7b6edd0826ce1340dfe8ee13c14164c674422427
Maycon <mayconleite06@yahoo.com.br>
2024-10-07 23:36:27 -0300

    formatação da coleta de dados

--- conta-banco/src/ContaTerminalJava.java
+++ conta-banco/src/ContaTerminalJava.java
@@ -3,13 +3,21 @@ import java.util.Locale;
 import java.util.Scanner;
 public class ContaTerminalJava {
     public static void main(String[] args) {
+        Double saldo = 0.02;
 
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite sua Agencia");
+        String agencia = scanner.next();
+    
         
+        System.out.println("Digite sua conta");
+        String conta = scanner.next();
 
-
-
+        
 
 
     }
